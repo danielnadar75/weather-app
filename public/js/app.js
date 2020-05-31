67084509f30041a133f8aa552f9b9e9dfcea16aa
Daniel Nadar <Daniel@Daniels-MacBook-Air.local>
2020-05-31 17:19:04 +0530

    removed console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('The client side javascript loaded!');
-
 const weatherForm = document.querySelector('form');
 const submit = document.querySelector('input');
 const messageOne = document.querySelector('#message-1')
