cb86fd7d1c4bc75d26920f6fe5fa8dbd0ec9b6a5
Daniel Nadar <Daniel@Daniels-MacBook-Air.local>
2020-05-31 17:26:31 +0530

    finalaised for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./util/geocode');
 const forecast = require('./util/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Serving port 3000');
+app.listen(port, () => {
+    console.log('Serving port ' + port);
 });
